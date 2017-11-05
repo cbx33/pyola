8593aeccdcc2489ffe603ae077d07c995baca4ec
psav <silentkeystroke@googlemail.com>
2017-11-05 20:17:10 +0000

    Added offset for sin/cos

--- pyola/modifiers/cos.py
+++ pyola/modifiers/cos.py
@@ -9,6 +9,7 @@ class CosineModifier(Modifier):
         super(CosineModifier, self).__init__(*args, **kwargs)
         self.amp = get_val_from_const(self.data.get('amp', None), self.manager.constants)
         self.freq = get_val_from_const(self.data.get('freq', None), self.manager.constants)
+        self.offset = get_val_from_const(self.data.get('offset', 0), self.manager.constants)
 
     def calculate(self):
-        return self.amp * math.cos(self.freq * (self.current_time))
+        return self.amp * math.cos((self.freq * self.current_time) + self.offset)
